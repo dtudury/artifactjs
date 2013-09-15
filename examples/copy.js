a39cc8bdb758d074dabd84ea65a582fb6b364ae6
David Tudury <david.tudury@voxer.com>
2013-09-14 21:16:17 -0700

    move to promises

---
+++ examples/copy.js
@@ -0,0 +1,33 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: davidtudury
+ * Date: 9/12/13
+ * Time: 8:37 PM
+ */
+
+
+var artifact = require('../lib/index');
+var Q = require('q');
+
+
+function log() {
+    console.log.apply(console, arguments);
+}
+
+
+Q()
+    .thenResolve('../temp/a/b/c').then(artifact.mkdir)
+    .thenResolve('../temp/d/b/c').then(artifact.mkdir)
+    .thenResolve('../temp/a/b/e').then(artifact.mkdir)
+    .thenResolve('../temp/a/b/f').then(artifact.mkdir)
+
+    .thenResolve('../temp').then(artifact.rm)
+
+    .done();
+
+
+
+
+var dm = new artifact.dependencyMap('copy.js', '../package.json');
+
+dm.isStale().then(log);
